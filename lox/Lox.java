1fe7257409bb4983842c7becd4d8f02558e5cb1c
Silas Maughan <silas.maughan@gmail.com>
2023-10-03 14:40:41 +0200

    lexer + scanner

--- lox/Lox.java
+++ lox/Lox.java
@@ -7,7 +7,6 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Scanner;
 
 public class Lox {
     static boolean hadError = false;
@@ -42,7 +41,7 @@ public class Lox {
     }
 
     private static void run(String source) {
-        Scanner scanner = new Scanner(source);
+        lox.Scanner scanner = new lox.Scanner(source);
         List<Token> tokens = scanner.scanTokens();
         // For now, just print the tokens.
         for (Token token : tokens) {
